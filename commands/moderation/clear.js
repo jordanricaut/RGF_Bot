5dfb9a812632f23f2cc8067ee9b706617d009eef
Jordan Ricaut <jordan.ricaut5@gmail.com>
2022-07-08 15:41:21 +0200

    Ajout groupe Summer league

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -8,13 +8,6 @@ module.exports = {
       type: "NUMBER",
       required: true,
     },
-    {
-      name: "user",
-      description:
-        "Sélectionner l'utilisateur pour la suppression des messages",
-      type: "USER",
-      required: false,
-    },
   ],
 
   async execute(client, interaction) {
@@ -23,29 +16,12 @@ module.exports = {
       return interaction.reply(
         "Le NOMBRE doit être inférieur à 100 et supérieur à 0"
       );
-    const target = interaction.options.getMember("user");
-
     const messageToDelete = await interaction.channel.messages.fetch();
-    if (target) {
-      let i = 0;
-      const filteredMessage = [];
-      (await messageToDelete).filter((msg) => {
-        if (msg.author.id == target.id && amount > i) {
-          filteredMessage.push(msg);
-          i++;
-        }
+    await interaction.channel.bulkDelete(amount, true).then((messages) => {
+      interaction.reply({
+        content: `J'ai supprimé ${messages.size} messages sur ce salon`,
+        ephemeral: true,
       });
-      await interaction.channel
-        .bulkDelete(filteredMessage, true)
-        .then((messages) => {
-          interaction.reply(
-            `J'ai supprimé ${messages.size} messages de l'utilisateur ${target}`
-          );
-        });
-    } else {
-      await interaction.channel.bulkDelete(amount, true).then((messages) => {
-        interaction.reply({ content: `J'ai supprimé ${messages.size} messages sur ce salon`, ephemeral: true });
-      });
-    }
+    });
   },
 };
